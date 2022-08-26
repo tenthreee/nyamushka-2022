8949c295099357d76ada02285500a95cd8084043
tenthreee <lena@htmlacademy.ru>
2022-08-26 19:47:43 +0300

    feat: add mocks

---
+++ src/mocks/items.js
@@ -0,0 +1,29 @@
+const items = [
+  {
+    id: 0,
+    flavor: "с фуа-гра",
+    quantity: "10 порций",
+    gift: "",
+    weight: "0,5",
+    describtion: "Печень утки разварная с артишоками.",
+  },
+  {
+    id: 0,
+    flavor: "с рыбой",
+    quantity: "40 порций",
+    gift: "",
+    weight: "2",
+    describtion: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+  },
+  {
+    id: 0,
+    flavor: "с курой",
+    quantity: "100 порций",
+    gift: "",
+    weight: "5",
+    happy: true,
+    describtion: "Филе из цыплят с трюфелями в бульоне.",
+  },
+];
+
+export default items;
